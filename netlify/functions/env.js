66fbb5458c9542518200297b94ddd53419220147
tedddy12eliana <97421819+Teddy12eliana@users.noreply.github.com>
2025-11-04 22:47:50 -0500

    Fixed env.js function and cleaned duplicates

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -9,7 +9,7 @@ export async function handler() {
       VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
       VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
       SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_KEY: process.env.SUPABASE_KEY, // for server funcs only
+      SUPABASE_KEY: process.env.SUPABASE_KEY, // for server-side use only
     }),
   };
 }
